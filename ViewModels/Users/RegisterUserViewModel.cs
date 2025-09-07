cff57f357bdbb7eb0e02f0836ea5a03e9adef67a
Dave <itschristiandavevilanofficial@gmail.com>
2025-09-07 09:56:02 +0800

    Added RegisterUserViewModel.cs

---
+++ ViewModels/Users/RegisterUserViewModel.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using Student_Performance_Tracker.Enums;
+
+public class RegisterUserViewModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Email address is required")]
+    [StringLength(255, ErrorMessage = "Email address cannot exceed 255 characters")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [StringLength(255, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Role is required")]
+    public Role Role { get; set; }
+}
